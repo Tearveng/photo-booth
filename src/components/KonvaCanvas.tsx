a1dc4f7db85afca76c541680e5faf88429f31559
Veng <vengtear1399@gmail.com>
2025-05-04 00:19:58 +0700

    udpate

--- src/components/KonvaCanvas.tsx
+++ src/components/KonvaCanvas.tsx
@@ -319,7 +319,7 @@ export default function KonvaCanvas(props: IKonCanvas) {
           variant="outlined"
           sx={{ fontSize: 12 }}
         >
-          {storeImg.length}
+          Download Photo stripe
         </Button>
         <Button
           sx={{ fontSize: 12 }}
