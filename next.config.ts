16e86be809316604780cc93553a74714c0d3ea87
Veng <vengtear1399@gmail.com>
2025-05-04 00:04:36 +0700

    udpate

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
-  env: {
-    NEXT_PUBLIC_API: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000',
-  },
+  // env: {
+  //   NEXT_PUBLIC_API: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000',
+  // },
   webpack(config, { isServer }) {
     // For client-side only, make `canvas` module resolve to an empty module
     if (!isServer) {
