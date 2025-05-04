049945a72473103637dc0bc709e378492e63eba4
Veng <vengtear1399@gmail.com>
2025-05-04 16:53:00 +0700

    update

--- src/components/Camera.tsx
+++ src/components/Camera.tsx
@@ -70,7 +70,7 @@ export default function Camera(props: ICamera) {
       if (context) {
         // Set canvas size to video size
         canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
+        canvas.height = video.videoHeight - 180;
 
         // Mirror effect if needed
         context.scale(-1, 1);
@@ -80,7 +80,7 @@ export default function Camera(props: ICamera) {
           -video.videoWidth,
           -180,
           video.videoWidth,
-          video.videoHeight
+          video.videoHeight - 180
         );
 
         // Get the image data URL
